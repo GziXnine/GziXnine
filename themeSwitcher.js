43be834b10695477a73e46108b5c5431bbd257ec
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-05-06 01:39:36 +0300

    Create themeSwitcher.js

---
+++ themeSwitcher.js
@@ -0,0 +1,22 @@
+<div align="center">
+  <p>
+    <a href="https://count.getloli.com/"><img src="https://count.getloli.com/get/@:AllamF5J"></a>
+  </p>
+  
+  <img id="snakeImage" src="" alt="Snake Gif">
+</div>
+
+<script>
+  // Assuming you have a variable named theme
+  var theme = "dark"; // or "white" based on your logic
+
+  // Select the image element
+  var snakeImage = document.getElementById("snakeImage");
+
+  // Set the image source based on the theme
+  if (theme === "dark") {
+    snakeImage.src = "https://github.com/AllamF5J/AllamF5J/blob/output/github-contribution-grid-snake-dark.svg";
+  } else {
+    snakeImage.src = "https://github.com/AllamF5J/AllamF5J/blob/output/github-contribution-grid-snake.svg";
+  }
+</script>
